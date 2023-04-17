fbf2f519923d795cb98f40990300f6313ebbdb8b
NFadil <75855353+NFadil@users.noreply.github.com>
2023-04-17 23:54:55 +0700

    menambah dasbiard

---
+++ Koneksi.php
@@ -0,0 +1,9 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_ppdb";
+    
+    //membuat koneksi
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+?>
---
+++ aksi_login.php
@@ -0,0 +1,22 @@
+<?php
+        session_start();
+        include "Koneksi.php";
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['username']) && isset($_POST['password'])) {
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        $sql = mysqli_query($conn,"select * from user where username='$user' and password='$pass'");
+        
+        $cek = mysqli_num_rows($sql);
+
+        // cek apakah username dan password ditemukan pada database
+        if($cek > 0){
+            // buat session login dan username
+            $_SESSION['username'] = $user;
+            // alihkan ke halaman dashboard admin
+            header("location:dashboard.php");
+
+            
+        }
+    }
+    ?>
